84e3bf411a965510a388ecfc0944407a0155d3d3
Roman <roman_kravets_den@mail.ru>
2024-02-17 18:08:54 +0300

    add model

---
+++ src/main/java/model/FilmEntity.java
@@ -0,0 +1,29 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name="film")
+public class FilmEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String year;
+    private String description;
+
+    @ManyToMany
+    @JoinTable(
+            name = "film_genre",
+            joinColumns = @JoinColumn(name="film_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id")
+    )
+    private Set<GenreEntity> genre = new HashSet<GenreEntity>();
+}
---
+++ src/main/java/model/GenreEntity.java
@@ -0,0 +1,25 @@
+package model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@Table(name="genre")
+public class GenreEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @ManyToMany
+    @JoinTable(
+            name = "film_genre",
+            joinColumns = @JoinColumn(name="genre_id"),
+            inverseJoinColumns = @JoinColumn(name = "film_id")
+    )
+    private Set<FilmEntity> genre = new HashSet<FilmEntity>();
+}
