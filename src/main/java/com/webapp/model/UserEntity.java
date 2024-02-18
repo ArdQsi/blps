9a9b11df8ca1cded58d69da7256d4eec8bd2bb4c
Denstep <denvvbaranov@gmail.com>
2024-02-18 03:15:06 +0300

    update

--- src/main/java/com/webapp/model/UserEntity.java
+++ src/main/java/com/webapp/model/UserEntity.java
@@ -1,21 +1,19 @@
 package com.webapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="user")
+@Table(name="_user")
 public class UserEntity {
     @Id
     @GeneratedValue
@@ -25,5 +23,8 @@ public class UserEntity {
     @Pattern(regexp = "^[a-zA-Z][a-zA-Z0-9]+@{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,4}$")
     private String email;
     private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private CardEntity card;
 }
 
--- src/main/java/com/webapp/service/FilmService.java
+++ src/main/java/com/webapp/service/FilmService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class FilmService {
-    @Autowired
+
     private FilmRepository filmRepository;
 
     public List<FilmEntity> getAllFilm() {
