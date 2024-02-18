91abb2ad4f983a9033cf629c98a0eba1b2ab1643
Denstep <denvvbaranov@gmail.com>
2024-02-18 17:41:10 +0300

    add card controller

--- src/main/java/com/webapp/controllers/PaymentController.java
+++ src/main/java/com/webapp/controllers/PaymentController.java
@@ -1,10 +1,30 @@
 package com.webapp.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.webapp.dto.CardDto;
+import com.webapp.dto.FilmDto;
+import com.webapp.exceptioin.NotFoundException;
+import com.webapp.model.FilmEntity;
+import com.webapp.service.CardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/pay")
+@RequiredArgsConstructor
+@RequestMapping("/rutube.ru")
 public class PaymentController {
+    private final CardService cardService;
 
+    @PostMapping("/pay")
+    public ResponseEntity<CardDto> saveCard(@RequestBody CardDto cardDto){
+        try{
+            cardService.saveCard(cardDto);
+        }
+        catch (NotFoundException e){
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(cardDto);
+    }
 }
