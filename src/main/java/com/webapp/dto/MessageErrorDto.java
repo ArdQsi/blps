9eac2ab1e4f6f3cd644370f22a00cfc9408080e1
Denstep <denvvbaranov@gmail.com>
2024-02-19 21:43:06 +0300

    fix message error

--- src/main/java/com/webapp/dto/MessageErrorDto.java
+++ src/main/java/com/webapp/dto/MessageErrorDto.java
@@ -1,30 +1,3 @@
 package com.webapp.dto;
 
-public class MessageErrorDto {
-    private int statusCode;
-    private String message;
-
-    public int getStatusCode() {
-        return statusCode;
-    }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public MessageErrorDto() {
-    }
-
-    public MessageErrorDto(int statusCode, String message) {
-        this.statusCode = statusCode;
-        this.message = message;
-    }
-}
+public record MessageErrorDto(int statusCode, String message) {}
